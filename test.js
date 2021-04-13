86308e3f6ab79081485247fcc68c5c20177ba7ac
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-04-13 16:55:57 -0400

    js homework

---
+++ test.js
@@ -0,0 +1,27 @@
+function addTwo(x,y){
+    answer = x+y
+    console.log(answer)
+}
+addTwo(2,4);
+addTwo(4,8)
+
+function subTwo(x,y){
+    answer= x-y;
+    console.log(answer)
+}
+subTwo(10,5)
+subTwo(20,10)
+
+function multTwo(x,y){
+    answer= x*y;
+    console.log(answer)
+}
+multTwo(4,6)
+multTwo(3,3)
+
+function divTwo(x,y){
+    answer=x/y;
+    console.log(answer)
+}
+    divTwo(4,2)
+    divTwo(10,2)
